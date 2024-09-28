b896d84d50584ea0aabaf8ce6174547d4dd4ce68
lapnguyencoder <qlml528@gmail.com>
2024-09-28 07:42:31 +0700

    Update boston.py

--- boston.py
+++ boston.py
@@ -1,14 +1,15 @@
+import streamlit as st
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
 usa = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 massachusetts = usa[(usa['name'] == 'United States') & (usa['iso_a2'] == 'US')]
+
 fig, ax = plt.subplots(figsize=(8, 8))
 massachusetts.plot(ax=ax, color='lightblue')
-
 ax.set_title('Bản đồ Tiểu bang Massachusetts', fontsize=15)
 ax.set_xlabel('Kinh độ')
 ax.set_ylabel('Vĩ độ')
 
-plt.show()
+st.pyplot(fig)
